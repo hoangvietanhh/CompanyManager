da31e67eabc5bd8696b9008c3f4b27d22ad5790f
HoangVietAnh <hoangvietanh1797@gmail.com>
2020-11-09 13:58:45 +0700

    add file

---
+++ src/Program/Main.java
@@ -0,0 +1,29 @@
+package Program;
+
+import entities.*;
+import methods.EmployeeManager;
+import methods.Information;
+import methods.Menu;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Admin admin = Admin.getInstance();
+
+        EmployeeManager employeeManager = new EmployeeManager();
+        Scanner input = new Scanner(System.in);
+        Menu menu = new Menu();
+        String code;
+        do {
+            System.out.println("------------Login-------------");
+            System.out.println("Enter employee code: ");
+            code = input.nextLine();
+
+        } while (!employeeManager.isContain(code) && !admin.getEmployeeCode().equals(code));
+        menu.menu();
+    }
+}
